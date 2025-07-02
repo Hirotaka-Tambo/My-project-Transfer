c685ea3c95c84848a6fe72ea772ab3241b1e0d9f
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-07-02 15:33:13 +0900

    sqlとのデータ連携があらかた完了

--- SpringTransferProject/src/main/java/com/example/transfer/entity/TransferRoute.java
+++ SpringTransferProject/src/main/java/com/example/transfer/entity/TransferRoute.java
@@ -1,6 +1,33 @@
 package com.example.transfer.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "transfer_route")
 public class TransferRoute {
-
+	
+	@Id //メインキー(DBのリーダー格==Primary Key)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String start_line;
+	private String start_station;
+	private Integer start_car;
+	private Integer start_door;
+	
+	private String via_line;
+	private String via_station;
+	private Integer via_car;
+	private Integer via_door;
+	
+	private String end_line;
+	private String end_station;
+	private Integer end_car;
+	private Integer end_door;
+	
+	private String destinationString;
 }
--- SpringTransferProject/src/main/java/com/example/transfer/repository/TransferRepository.java
+++ SpringTransferProject/src/main/java/com/example/transfer/repository/TransferRepository.java
@@ -2,6 +2,8 @@ package com.example.transfer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.transfer.entity.TransferRoute;
+
 public interface TransferRepository extends JpaRepository<TransferRoute, Integer> {
 	    // JpaRepositoryを継承することで、基本的なCRUD操作（保存、検索など）が自動的に提供される
 
