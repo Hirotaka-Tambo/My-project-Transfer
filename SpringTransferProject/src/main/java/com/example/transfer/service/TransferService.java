1565378f0b50a511ee34b20658e015e7cb62d89f
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-07-12 16:29:37 +0900

    serviceの記述途中

--- SpringTransferProject/src/main/java/com/example/transfer/service/TransferService.java
+++ SpringTransferProject/src/main/java/com/example/transfer/service/TransferService.java
@@ -1,7 +1,10 @@
 package com.example.transfer.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.transfer.exception.BusinessValidationException;
+import com.example.transfer.form.RegistForm;
 import com.example.transfer.repository.TransferRepository;
 
 
@@ -11,6 +14,7 @@ import com.example.transfer.repository.TransferRepository;
  *　　　　　　　　　　お気に入り登録の上限や例外処理、バリデーションなど、
  *                 サービス提供においての「ルールブック」のようなもの。  
 */
+
 
 @Service
 public class TransferService {
@@ -28,6 +32,41 @@ public class TransferService {
 	public TransferService(TransferRepository transferRepository) {
 		this.transferRepository = transferRepository;
 	}
+	
+	/*
+	 * 乗り換え情報の登録用のロジック
+	 * フォームデータ　ーー　エンティティに変換 ーー　ビジネスバリデーションエクセプションで正誤判定 ーー ようやくDBに保存する
+	 * @param ーー　form 登録フォームデータ
+	 * @throws ーー BuisnessValidationExceptionがエラー出た場合の例外処理
+	 */
+	
+	@Transactional
+	public void registerTransfer(RegistForm form)throws BusinessValidationException{
+		/*
+		 * ビジネスロジックのバリデーション
+		 * 出発駅と到着駅のチェック 同じ駅になっていないかの確認
+		 */
+		if(form.getStartStation() != null && form.getStartStation().equals(form.getEndStation())) {
+			throw new BusinessValidationException("出発駅と到着駅は異なる駅を入力してください");
+		}
+		/*
+		 * 経由駅のチェック①出発駅および到着駅とそれぞれ異なる駅であることの確認
+		 * [注意事項]経由駅はnullでもいいと設定したため、nullチェックをスルーできるようにしないといけない
+		 * つまり、nullやisEmptyでない時(何かしらの入力がされている場合)に初めて、判定を行う
+		 */
+		if(form.getViaStation() != null && !form.getViaStation().isEmpty()) {
+			if(form.getViaStation().equals(form.getStartStation()) || form.getViaStation().equals(form.getEndStation())) {
+				throw new BusinessValidationException("経由駅は、出発地と到着地と異なる駅を入力してください");
+			}
+		}
+		
+		/*
+		 * 目的地のチェック 
+		 */
+	}
+	
+	
+	
 	
 	
 
