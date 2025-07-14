2f9b7b5825df839dcf4c538acca90ad84c9c5337
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-07-14 13:14:21 +0900

    test(まだ作業中)

--- SpringTransferProject/src/main/java/com/example/transfer/controller/RegistController.java
+++ SpringTransferProject/src/main/java/com/example/transfer/controller/RegistController.java
@@ -6,10 +6,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.transfer.form.RegistForm;
+import com.example.transfer.service.TransferService;
 
 
 @Controller
 public class RegistController {
+	
+	private final TransferService transferService;
+	
+	// コンストラクタインジェクション(依存性)
+	
+	public RegistController(TransferService transferService){
+		this.transferService = transferService;
+	}
+	
+	/*
+	 * GETリクエストで乗り換え登録画面の初期表示。(ユーザーが入力できるように最初の画面を持ってくる。)
+	 * 新しいRegistFormのオブジェクト(型)をモデルに追加して、画面を表示する。
+	 * @param
+	 * @return
+	 */
 	
 	/*乗り換え登録画面の前準備(必要な情報を探してくる)をリクエスト*/
 	@GetMapping("/regist-transfer")
